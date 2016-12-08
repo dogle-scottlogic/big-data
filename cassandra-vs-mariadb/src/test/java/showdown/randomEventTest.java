fadccbfd0416d8d320e55bb243ccadc0848a08c1
SCOTT\dogle <dogle@scottlogic.co.uk>
2016-12-08 16:16:08 +0000

    add create event tests

--- cassandra-vs-mariadb/src/test/java/showdown/randomEventTest.java
+++ cassandra-vs-mariadb/src/test/java/showdown/randomEventTest.java
@@ -52,23 +52,6 @@ public class randomEventTest {
     }
 
     @Test
-
-    public void FiveThousandCreateEvents() throws Exception {
-        Settings.setStringSetting("EVENT_GEN_MODE", "fixed");
-        Settings.setIntSetting("ORDER_CACHE_SIZE", 2500);
-        Settings.setIntSetting("NUM_FIXED_EVENTS", 500);
-        int numOfEvents = 2500;
-
-        // Cassandra
-        CassandraDBStorer cdbs = new CassandraDBStorer();
-        Conveyor.processEventsWithLog(numOfEvents, new Enums.EventType[]{}, cdbs, "fiveThousandFixedEventsCassandra");
-
-        //Maria
-        MariaDBStorer mdbs = new MariaDBStorer();
-        Conveyor.processEventsWithLog(numOfEvents, new Enums.EventType[]{}, mdbs, "fiveThousandFixedEventsMariaDB");
-    }
-
-    @Test
     public void TenThousandEventsAgainstVaryingWorkingSetSizes() throws Exception {
         Settings.setStringSetting("EVENT_GEN_MODE", "fixed");
         Settings.setIntSetting("ORDER_CACHE_SIZE", 1000);
